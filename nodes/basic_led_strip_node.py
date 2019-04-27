3bd4928b12e4d5110969828be43cc18ad314c5e7
francescavponce <fponce@caltech.edu>
2019-04-26 17:09:49 -0700

    Added launch file. A few other minor tweaks.y

--- nodes/basic_led_strip_node.py
+++ nodes/basic_led_strip_node.py
@@ -12,17 +12,21 @@ class BasicLedStripNode(object):
 
     def __init__(self):
         rospy.init_node('basic_led_strip')
-        self.port = rospy.get_param('port', '/dev/ttyUSB0')
+        self.port = rospy.get_param('/basic_led_strip/port', '/dev/ttyUSB1')
         self.led_strip = BasicLedStrip(self.port)
         self.led_strip.off()
         self.led_value_sub = rospy.Subscriber('set_led_value', LEDValue, self.set_led_value_callback)
 
     def set_led_value_callback(self,msg):
-        self.led_strip.set(msg.led_number, (msg.red, msg.green, msg.blue), mode='exclusive')
+        self.led_strip.set(msg.led_number, (msg.red, msg.green, msg.blue), mode='inclusive')
 
     def run(self):
         while not rospy.is_shutdown():
             rospy.sleep(0.1)
+            
+    def all_off(self):
+        self.led_strip = BasicLedStrip(self.port)
+        self.led_strip.off()
 
 
 # -------------------------------------------------------------------------------------------------
